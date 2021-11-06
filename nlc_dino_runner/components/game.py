cfa1650c8868990d7d4adbed74331ff3cfba67ca
Valeria-Rico <valerico0306@gmail.com>
2021-11-05 20:28:49 -0400

    Class3: Dino
    Creamos un pygame file, "dinosaur", en el cual creamos una clase. En ella creamos los métodos correr, agachar y saltar. Para que se pueda ver el efecto del dinosaurio corriendo, utilizamos dos imágenes, las cuales de forma intercalada simula que el mismo esté corriendo. Para agachar y saltar, pusimos un k_down y k_up, que en el momento en el que el usuario presiona dichas teclas, el dinosaurio saltará y se agachará respectivamente.

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,28 +1,20 @@
 import pygame
 
-from nlc_dino_runner.utils.constants import TITTLE
-
-from nlc_dino_runner.utils.constants import ICON
-
-from nlc_dino_runner.utils.constants import BG
-
-from nlc_dino_runner.utils.constants import FPS
-
-from nlc_dino_runner.utils.constants import SCREEN_WIDTH
-
-from nlc_dino_runner.utils.constants import SCREEN_HEIGHT
+from nlc_dino_runner.utils.constants import TITTLE,ICON, BG, FPS, SCREEN_WIDTH, SCREEN_HEIGHT
+from nlc_dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.playing = False
         pygame.display.set_caption(TITTLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.x_pos_bg = 0
         self.y_pos_bg = 400
+        self.playing = False
         self.game_speed = 20
         self.clock = pygame.time.Clock()
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -38,13 +30,17 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
+        pygame.display.update()
         pygame.display.flip()
+
 
     def draw_background(self):
         image_with = BG.get_width()
